d2ab232cf4b3178ced5998569137f592d9d787f0
Martin Mamani Napan <a20204009@idat.edu.pe>
2022-08-09 21:34:13 -0500

    eureka

---
+++ src/main/java/pe/idat/edu/EurekaServerEjemploApplication.java
@@ -0,0 +1,15 @@
+package pe.idat.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerEjemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerEjemploApplication.class, args);
+	}
+
+}
